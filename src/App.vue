70b591203552aacd99dd2652110a66f820431b5f
DESKTOP-GVI4G4U\Kwatog <Kwatog>
2024-04-01 22:24:34 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import MainTemplate from './components/templates/main-template.vue'
+</script>
+
+<template>
+  <div class="main-container">
+    <div class="login-container">
+      <form @submit.prevent="login">
+        <div class="login-logo">
+        <img src="./assets/images/logo.png" alt="Logo" style="width: 250px; height: 250px;">
+      </div>
+        <input type="text" id="username" name="username" v-model="username" required>
+        <input type="password" id="password" name="password" v-model="password" required>
+        <button type="submit">Login</button>
+        <div>
+          <a href="#" class="forgot-password-link">Forgot Password?</a>
+        </div>
+      </form>
+      <div>
+          <a class="login-footer">Copyright</a>
+      </div>
+    </div>
+    <div class="logo-container">
+      <img src="./assets/images/ctu-logo.png" alt="Logo">
+    </div>
+  </div>
+</template> 
