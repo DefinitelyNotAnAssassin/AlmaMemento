31ed9e29f5ab9bb79bdc7fcf23cc1d78aa3adda8
Emmanuel Ugaban <>
2024-04-17 22:59:37 +0800

    SideBar

--- src/views/alumni.vue
+++ src/views/alumni.vue
@@ -2,14 +2,21 @@
   <div class="main">
     <div class="container-fluid bg-dark p-0">
       <NavBar />
-      <SideBar />
+      <div class="container-fluid d-flex">
+        <SideBar />
+        <div class="main-content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+            quidem rem at ab, laboriosam sit quasi quos eius nihil itaque!
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import NavBar from './alumni-components/alumni-navbar.vue';
-import SideBar from './alumni-components/alumni-sidebar.vue';
-
+import { ref } from "vue";
+import NavBar from "./alumni-components/alumni-navbar.vue";
+import SideBar from "./alumni-components/alumni-sidebar.vue";
 </script>
