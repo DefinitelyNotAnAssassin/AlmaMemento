dcf820dfcb9c9dc5977a28ec9877889392788cab
Emmanuel Ugaban <>
2024-04-17 23:30:37 +0800

    SideBar

--- src/views/alumni-components/alumni-sidebar.vue
+++ src/views/alumni-components/alumni-sidebar.vue
@@ -1,8 +1,9 @@
 <template>
   <aside
-    class="sidebar-container bg-secondary d-flex flex-column align-items-center justify-content-center"
+    class="sidebar-container bg-secondary d-flex flex-column align-items-center p-3"
   >
     <img
+      class="mt-5"
       src="https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/362681859_833846268097031_774316903881338625_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHybu1WboC1y_yed82N84fp01fI_da8ksXTV8j91rySxXf65cUC0wq7qeoTuGVHZoA7wWJE_7z3FS0NZzVf5bJJ&_nc_ohc=8WQTYxhl5nQAb5u3wwK&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfBm__PVH81knV7GG_RFs3itnIRxD5TSyT79WZ3tDsMZaw&oe=6625BCA4"
       alt="profile"
     />
@@ -48,6 +49,6 @@ aside img {
 }
 
 .td-padding-left {
-    padding-left: 20px;
+  padding-left: 20px;
 }
 </style>
