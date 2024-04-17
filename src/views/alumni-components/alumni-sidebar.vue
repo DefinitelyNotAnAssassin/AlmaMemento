0e2a11e08f82e0b7fa186d8653ff27446922b1f6
Emmanuel Ugaban <>
2024-04-17 22:47:22 +0800

    Side Bar

---
+++ src/views/alumni-components/alumni-sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+    <aside class="sidebar-container">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Ffacts%2Fdomestic-dog&psig=AOvVaw0nhSJRnBGSbX55CgyUlaYQ&ust=1713451358115000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDhw-69yYUDFQAAAAAdAAAAABAE" alt="profile">
+        <h3>Emmanuel Ugaban</h3>
+        <p>emman@gmail.com</p>
+        <div>
+            <h3>Profile</h3>
+            <div>
+                <span>ID number: </span>
+                <span>00000</span>
+            </div>
+            <div>
+                <span>Course: </span>
+                <span>BSIT</span>
+            </div>
+            <div>
+                <span>Class Year: </span>
+                <span>2023-2024</span>
+            </div>
+            <div>
+                <span>Phone </span>
+                <span>09123456789</span>
+            </div>
+        </div>
+    </aside>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- src/views/alumni.vue
+++ src/views/alumni.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="main">
     <NavBar />
+    <SideBar />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import NavBar from './alumni-components/alumni-navbar.vue';
+import SideBar from './alumni-components/alumni-sidebar.vue';
 
 </script>
