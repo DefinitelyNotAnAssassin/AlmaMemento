1f5897150e5140e117047989fa736e46fd2b5d3a
Emmanuel Ugaban <>
2024-04-17 19:12:58 +0800

    Nav Bar

---
+++ src/views/alumni-components/alumni-navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navbar-container">
+        <img class="logo" src="../../assets/images/logo.png" alt="Logo">
+        <div class="navbar-buttons">
+            <button class="btn-home">Home</button>
+            <button class="btn-yearbook">Yearbook</button>
+        </div>
+        <div>
+            <div class="search-container">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" placeholder="Search...">
+            </div>
+            <button class="btn-notification"><i class="fa-solid fa-bell"></i></button>
+            <button class="btn-profile"><i class="fa-solid fa-user"></i></button>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
--- src/views/alumni.vue
+++ src/views/alumni.vue
@@ -1,1 +1,11 @@
-<template>Alumni</template>
+<template>
+  <div class="main">
+    <div>
+        <NavBar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './alumni-components/alumni-navbar.vue';
+</script>
