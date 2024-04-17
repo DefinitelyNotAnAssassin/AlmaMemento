259b6480a5a3dd28dac71889cfa44a6bd3a9bff5
Emmanuel Ugaban <>
2024-04-17 23:24:53 +0800

    SideBar

--- src/views/alumni-components/alumni-sidebar.vue
+++ src/views/alumni-components/alumni-sidebar.vue
@@ -1,28 +1,30 @@
 <template>
-  <aside class="sidebar-container bg-secondary d-flex flex-column align-items-center justify-content-center">
+  <aside
+    class="sidebar-container bg-secondary d-flex flex-column align-items-center justify-content-center"
+  >
     <img
       src="https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/362681859_833846268097031_774316903881338625_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHybu1WboC1y_yed82N84fp01fI_da8ksXTV8j91rySxXf65cUC0wq7qeoTuGVHZoA7wWJE_7z3FS0NZzVf5bJJ&_nc_ohc=8WQTYxhl5nQAb5u3wwK&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfBm__PVH81knV7GG_RFs3itnIRxD5TSyT79WZ3tDsMZaw&oe=6625BCA4"
       alt="profile"
     />
-    <h3>Emmanuel Ugaban</h3>
+    <h4 class="mt-2">Emmanuel Ugaban</h4>
     <p>emman@gmail.com</p>
-    <div>
-      <h3>Profile</h3>
+    <div class="mt-2">
+      <h4>Profile</h4>
       <div>
         <span>ID number: </span>
-        <span>00000</span>
+        <span class="margin-left">00000</span>
       </div>
       <div>
         <span>Course: </span>
-        <span>BSIT</span>
+        <span class="margin-left">BSIT</span>
       </div>
       <div>
         <span>Class Year: </span>
-        <span>2023-2024</span>
+        <span class="margin-left">2023-2024</span>
       </div>
       <div>
         <span>Phone </span>
-        <span>09123456789</span>
+        <span class="margin-left">09123456789</span>
       </div>
     </div>
   </aside>
@@ -39,7 +41,11 @@ aside {
 }
 
 aside img {
-    height: 100px;
-    widows: 100px;
+  height: 100px;
+  widows: 100px;
+}
+
+.margin-left {
+  margin-left: 20px;
 }
 </style>
