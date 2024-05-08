d1b912ea752e81b911e2898619e95cc015a6d6e9
Emmanuel Ugaban <>
2024-05-08 16:41:28 +0800

    .

--- src/views/admin-components/graduation-album-components/graduation-album-graduation-portrait.vue
+++ src/views/admin-components/graduation-album-components/graduation-album-graduation-portrait.vue
@@ -80,7 +80,7 @@
             </button>
           </div>
         </div>
-        <div class="text-center" style="font-size: 12px">
+        <div class="text-center" style="width: 150px; font-size: 12px">
           <div class="bg-dark text-light">{{ student.name }}</div>
           <div v-if="student.address">{{ student.address }}</div>
           <div style="height: 30px; overflow-y: auto" v-if="student.quotes">
