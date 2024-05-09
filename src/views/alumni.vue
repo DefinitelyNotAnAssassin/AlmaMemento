6ac5d31daf83e040c2af2d3f688c96b304358c84
Emmanuel Ugaban <>
2024-05-09 18:19:35 +0800

    .

--- src/views/alumni.vue
+++ src/views/alumni.vue
@@ -105,7 +105,7 @@
             class="bg-secondary"
             style="
               width: calc(100% - 400px);
-              height: calc(100vh - 200px);
+              height: calc(100vh - 100px);
               overflow-y: auto;
             "
           >
