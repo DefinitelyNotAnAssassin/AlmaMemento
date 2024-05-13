067743221602dec29131d0528990a735b4f25e0f
Emmanuel Ugaban <>
2024-05-13 20:56:41 +0800

    .

--- src/views/alumni-components/alumni-navbar.vue
+++ src/views/alumni-components/alumni-navbar.vue
@@ -1,14 +1,33 @@
 <template>
   <nav class="navbar">
     <div class="navbar-brand">
-      <a class="navbar-item" href="#">
-      </a>
+      <a class="navbar-item" href="#"> </a>
     </div>
     <div>
-      <router-link :to="{ path: '/alumniDashboard', query: { userId: userId } }" class="navbar-item text-light">Home</router-link>
-      <router-link :to="{ path: '/yearbook', query: { userId: userId } }" class="navbar-item text-light">Yearbook</router-link>
-      <router-link :to="{ path: '/memento', query: { userId: userId } }" class="navbar-item text-light">Memento</router-link>
-      <router-link :to="{ path: '/contact', query: { userId: userId } }" class="navbar-item text-light">Contact</router-link>
+      <router-link
+        :to="{ path: '/alumniDashboard', query: { userId: userId } }"
+        class="navbar-item text-light"
+        style="font-size: 15px; text-decoration: none"
+        >Home</router-link
+      >
+      <router-link
+        :to="{ path: '/yearbook', query: { userId: userId } }"
+        class="navbar-item text-light"
+        style="font-size: 15px; text-decoration: none"
+        >Yearbook</router-link
+      >
+      <router-link
+        :to="{ path: '/memento', query: { userId: userId } }"
+        class="navbar-item text-light"
+        style="font-size: 15px; text-decoration: none"
+        >Memento</router-link
+      >
+      <router-link
+        :to="{ path: '/contact', query: { userId: userId } }"
+        class="navbar-item text-light"
+        style="font-size: 15px; text-decoration: none"
+        >Contact</router-link
+      >
     </div>
     <div class="navbar-end">
       <a class="navbar-item text-light" @click="toggleNotifications">
@@ -21,22 +40,16 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
 const router = useRouter();
 const userId = computed(() => router.currentRoute.value.query.userId);
 
-const notificationsVisible = ref(false)
+const notificationsVisible = ref(false);
 
 const toggleNotifications = () => {
-  notificationsVisible.value = !notificationsVisible.value
-}
-
+  notificationsVisible.value = !notificationsVisible.value;
+};
 </script>
 
-<style scoped>
-router-link {
-  text-decoration: none;
-  font-size: 15px;
-}
-</style>
+<style scoped></style>
