f37d356869668227afdf0a7dec78da2570b1687b
Emmanuel Ugaban <>
2024-05-15 10:14:45 +0800

    Contact support

--- src/views/admin-components/admin-contact-components/admin-contact-main.vue
+++ src/views/admin-components/admin-contact-components/admin-contact-main.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+  <div class="components-page-main-container p-3">
+    <div class="d-flex justify-content-between">
       <input
         class="search-bar form-control"
         style="width: 250px"
@@ -14,70 +15,74 @@
       >
         Delete
       </button>
-      <ul class="list-group">
-        <li v-for="message in messages" :key="message.id" class="list-group-item">
-          <div class="d-flex justify-content-between align-items-center">
-            <input type="checkbox" v-model="selectedMessages" :value="message.id" />
-            <div @click="navigateToMessage(message.id)" style="cursor: pointer;">
-              <div>{{ message.name }}</div>
-              <div>{{ message.subject }}</div>
-              <div>{{ message.date }}</div>
-            </div>
-          </div>
-        </li>
-      </ul>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, defineEmits } from 'vue';
-  import { db } from "../../../firebase/index.js";
-  import {
-    collection,
-    query,
-    where,
-    getDocs,
-    addDoc,
-    updateDoc,
-    doc,
-    deleteDoc,
-    onSnapshot,
-  } from "firebase/firestore";
-  
-  const messages = ref([]);
-  const selectedMessages = ref([]);
-  const searchQuery = ref("");
-  const currentPage = ref("Contact");
-  
-  const fetchMessages = async () => {
-    const q = query(collection(db, 'concerns'));
-    const querySnapshot = await getDocs(q);
-    messages.value = querySnapshot.docs.map(doc => ({
-      id: doc.id,
-      name: doc.data().name,
-      subject: doc.data().subject,
-      date: doc.data().date,
-    }));
-  };
-  
-  const confirmDelete = async () => {
-    if (confirm("Are you sure you want to delete the selected messages?")) {
-      for (const id of selectedMessages.value) {
-        const docRef = doc(db, "concerns", id);
-        await deleteDoc(docRef);
-      }
-      selectedMessages.value = [];
-    }
-  };
+    <ul class="list-group">
+      <li v-for="message in messages" :key="message.id" class="list-group-item">
+        <div class="d-flex justify-content-between align-items-center">
+          <input
+            type="checkbox"
+            v-model="selectedMessages"
+            :value="message.id"
+          />
+          <div @click="navigateToMessage(message.id)" style="cursor: pointer">
+            <div>{{ message.name }}</div>
+            <div>{{ message.subject }}</div>
+            <div>{{ message.date }}</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
 
-  const emit = defineEmits(["update:currentPage"]);
-  
-  const navigateToMessage = (id) => {
-    currentPage.value = "Contact Message";
-    emit("update:currentPage", "Contact Message");
-    emit("id", id);
-  };
-  
-  fetchMessages();
-  </script>
-  
+<script setup>
+import { ref, defineEmits } from "vue";
+import { db } from "../../../firebase/index.js";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  onSnapshot,
+} from "firebase/firestore";
+
+const messages = ref([]);
+const selectedMessages = ref([]);
+const searchQuery = ref("");
+const currentPage = ref("Contact");
+
+const fetchMessages = async () => {
+  const q = query(collection(db, "concerns"));
+  const querySnapshot = await getDocs(q);
+  messages.value = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    name: doc.data().name,
+    subject: doc.data().subject,
+    date: doc.data().date,
+  }));
+};
+
+const confirmDelete = async () => {
+  if (confirm("Are you sure you want to delete the selected messages?")) {
+    for (const id of selectedMessages.value) {
+      const docRef = doc(db, "concerns", id);
+      await deleteDoc(docRef);
+    }
+    selectedMessages.value = [];
+  }
+};
+
+const emit = defineEmits(["update:currentPage"]);
+
+const navigateToMessage = (id) => {
+  currentPage.value = "Contact Message";
+  emit("update:currentPage", "Contact Message");
+  emit("id", id);
+};
+
+fetchMessages();
+</script>
