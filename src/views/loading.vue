fdea5345a4ef93b0bb9e4e32a60ea19cd3e59cc3
Emmanuel Ugaban <>
2024-05-20 03:00:54 +0800

    .

--- src/views/loading.vue
+++ src/views/loading.vue
@@ -2,16 +2,18 @@
   <div class="loader"></div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
 .loader {
+  position: absolute;
+  top: 0;
+  left: 0;
   border: 16px solid #f3f3f3;
   border-top: 16px solid #3498db;
   border-radius: 50%;
-  width: 120px;
-  height: 120px;
+  width: 100vw;
+  height: 100vh;
   animation: spin 2s linear infinite;
 }
 
