dd350c5b35d9d64fd0e104bff54531919f96ba59
Emmanuel Ugaban <>
2024-05-19 20:38:23 +0800

    .

--- src/views/alumni.vue
+++ src/views/alumni.vue
@@ -10,10 +10,15 @@
               class="background-color-brown card m-3 p-2 pb-5"
               style="position: relative; width: 400px"
             >
-              <div class="d-flex">
+              <div class="d-flex align-items-center">
                 <img
                   src="https://scontent.fmnl4-6.fna.fbcdn.net/v/t39.30808-6/441204475_122128868540246120_5470515469793099504_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEHdxMJCaCsLsFSM-wYtFe3XosaZDEWO_VeixpkMRY79TJ_9UeU8ZAtSnOblRBciZ_8Q9QicYbJldRWKiq41WfR&_nc_ohc=IbNiD3P1d_0Q7kNvgEA5qtT&_nc_ht=scontent.fmnl4-6.fna&oh=00_AYAJDf4u4ynNxOHDxctLsm2DsduxRh4zfnORs09d16hCmw&oe=664FB891"
-                  style="height: 50px; width: 50px; border-radius: 50%"
+                  style="
+                    height: 50px;
+                    width: 50px;
+                    border-radius: 50%;
+                    margin-right: 10px;
+                  "
                 />
                 <input
                   type="text"
@@ -23,8 +28,8 @@
               </div>
               <button
                 @click="showPostModal"
-                class="btn m-2"
-                style="position: absolute; bottom: 10px; right: 10px"
+                class="btn m-2 text-light"
+                style="position: absolute; bottom: 0; right: 0"
               >
                 <i class="bi bi-card-image"></i> Photo
               </button>
