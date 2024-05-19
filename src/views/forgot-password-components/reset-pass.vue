c80d0b89d87ec3365763565cb2df15708a334d21
Emmanuel Ugaban <>
2024-05-20 04:29:34 +0800

    .

--- src/views/forgot-password-components/reset-pass.vue
+++ src/views/forgot-password-components/reset-pass.vue
@@ -1,44 +1,55 @@
 <template>
-    <div>
-      <h1>Reset Password</h1>
+  <div class="bg-secondary">
+    <div class="d-flex flex-row align-items-center">
+      <img src="../../assets/images/logo2.png" />
+      <h3>Reset Password</h3>
       <form @submit.prevent="checkId">
         <div>
-          <label for="idNumber">ID Number:</label>
-          <input type="text" v-model="idNumber" id="idNumber" required />
+          <label for="idNumber">ID Number</label>
+          <input
+            class="form-control"
+            type="text"
+            v-model="idNumber"
+            id="idNumber"
+            required
+          />
         </div>
-        <button type="submit">Submit</button>
+        <p class="text-danger" v-if="errorMessage">{{ errorMessage }}</p>
+        <button class="btn btn-danger mt-3" type="submit">Submit</button>
       </form>
-      <p v-if="errorMessage">{{ errorMessage }}</p>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, defineEmits } from 'vue';
-  import { collection, query, where, getDocs } from 'firebase/firestore';
-  import { db } from '../../firebase/index.js';
-  
-  const idNumber = ref('');
-  const errorMessage = ref('');
-  const emit = defineEmits(["update:currentPage", "id"]);
-  const currentPage = ref("Reset");
-  
-  const checkId = async () => {
-    try {
-        const q = query(collection(db, 'users'), where('alumnaID', '==', idNumber.value));
-        const querySnapshot = await getDocs(q);
-      
-      if (!querySnapshot.empty) {
-        errorMessage.value = '';
-        currentPage.value = "Verify";
-        emit("update:currentPage", "Verify");
-        emit("id", idNumber.value);
-      } else {
-        errorMessage.value = 'ID number does not exist';
-      }
-    } catch (error) {
-      console.error('Error checking ID:', error);
-      errorMessage.value = 'An error occurred while checking the ID';
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from "vue";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/index.js";
+
+const idNumber = ref("");
+const errorMessage = ref("");
+const emit = defineEmits(["update:currentPage", "id"]);
+const currentPage = ref("Reset");
+
+const checkId = async () => {
+  try {
+    const q = query(
+      collection(db, "users"),
+      where("alumnaID", "==", idNumber.value)
+    );
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      errorMessage.value = "";
+      currentPage.value = "Verify";
+      emit("update:currentPage", "Verify");
+      emit("id", idNumber.value);
+    } else {
+      errorMessage.value = "ID number does not exist";
     }
-  };
-  </script>
-  
+  } catch (error) {
+    console.error("Error checking ID:", error);
+    errorMessage.value = "An error occurred while checking the ID";
+  }
+};
+</script>
