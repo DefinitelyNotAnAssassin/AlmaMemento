d31742d6b8dec0932eea60f28eb6164818226e2f
Emmanuel Ugaban <>
2024-05-20 02:58:45 +0800

    Loading

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,72 +1,113 @@
 <template>
-    <div class="main-container">
-      <div class="login-container d-flex flex-column align-items-center justify-content-between">
-        <form class="mt-5" @submit.prevent="signin">
-          <div class="login-logo">
-            <img src="@/assets/images/logo2.png" alt="Logo" style="width: 250px; height: 250px;">
-          </div>
-          <input type="text" id="alumniID" name="alumniID" v-model="alumniID" required placeholder="Alumni ID">
-          <input type="password" id="password" name="password" v-model="password" required placeholder="Password">
-          <p v-if="errMsg">{{ errMsg }}</p>
-          <button type="submit">Login</button>
-          <div>
-          <router-link to="/forgotPassword" class="forgot-password-link">Forgot Password?</router-link>
+  <Loading />
+  <div class="main-container">
+    <div
+      class="login-container d-flex flex-column align-items-center justify-content-between"
+    >
+      <form class="mt-5" @submit.prevent="signin">
+        <div class="login-logo">
+          <img
+            src="@/assets/images/logo2.png"
+            alt="Logo"
+            style="width: 250px; height: 250px"
+          />
         </div>
-        </form>
-        <div class="mb-2">
-          <a class="login-footer text-dark">Copyright &copy; 2023 AlmaMemento. All Rights Reserved.</a>
+        <input
+          type="text"
+          id="alumniID"
+          name="alumniID"
+          v-model="alumniID"
+          required
+          placeholder="Alumni ID"
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          v-model="password"
+          required
+          placeholder="Password"
+        />
+        <p v-if="errMsg">{{ errMsg }}</p>
+        <button type="submit">Login</button>
+        <div>
+          <router-link to="/forgotPassword" class="forgot-password-link"
+            >Forgot Password?</router-link
+          >
         </div>
-      </div>
-      <div class="logo-container">
-        <img src="@/assets/images/ctu-logo.png" alt="Logo">
+      </form>
+      <div class="mb-2">
+        <a class="login-footer text-dark"
+          >Copyright &copy; 2023 AlmaMemento. All Rights Reserved.</a
+        >
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { useRouter } from 'vue-router'
-  import { db } from '../firebase/index.js'
-  import { collection, getDocs, updateDoc, doc, query, where } from 'firebase/firestore'
-  
-  const alumniID = ref("")
-  const password = ref("")
-  const errMsg = ref("")
-  
-  const router = useRouter()
-  
-  const signin = async () => {
-    try {
-      console.log("Trying to sign in...")
-  
-      const q = query(collection(db, "users"), where("alumnaID", "==", alumniID.value), where("alumna_password", "==", password.value));
-      const querySnapshot = await getDocs(q);
-      const user = querySnapshot.docs[0];
-  
-      if (user) {
-        console.log("User found:", user.data())
-  
-        if (user.data().status === 'active') {
-          await updateDoc(doc(db, 'users', user.id), { loggedIn: true });
-  
-          if (user.data().userlevel === 'administrator') {
-            router.push({ name: 'adminDashboard', query: { userId: user.id } })
-          } else if(user.data().userlevel === 'alumni') {
-            router.push({ name: 'alumniDashboard', query: { userId: user.id, alumniId: user.data().alumnaID } })
-          } else if(user.data().userlevel === 'moderator') {
-            router.push({ name: 'modDashboard', query: { userId: user.id } })
-          }
-  
-          console.log("Current URL:", window.location.href);
-        } else {
-          errMsg.value = "Your account has been deactivated"
+    <div class="logo-container">
+      <img src="@/assets/images/ctu-logo.png" alt="Logo" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { db } from "../firebase/index.js";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+import Loading from "./loading.vue";
+
+const alumniID = ref("");
+const password = ref("");
+const errMsg = ref("");
+
+const router = useRouter();
+
+const signin = async () => {
+  try {
+    console.log("Trying to sign in...");
+
+    const q = query(
+      collection(db, "users"),
+      where("alumnaID", "==", alumniID.value),
+      where("alumna_password", "==", password.value)
+    );
+    const querySnapshot = await getDocs(q);
+    const user = querySnapshot.docs[0];
+
+    if (user) {
+      console.log("User found:", user.data());
+
+      if (user.data().status === "active") {
+        await updateDoc(doc(db, "users", user.id), { loggedIn: true });
+
+        if (user.data().userlevel === "administrator") {
+          router.push({ name: "adminDashboard", query: { userId: user.id } });
+        } else if (user.data().userlevel === "alumni") {
+          router.push({
+            name: "alumniDashboard",
+            query: { userId: user.id, alumniId: user.data().alumnaID },
+          });
+        } else if (user.data().userlevel === "moderator") {
+          router.push({ name: "modDashboard", query: { userId: user.id } });
         }
+
+        console.log("Current URL:", window.location.href);
       } else {
-        errMsg.value = "No account with that alumni number and password was found"
+        errMsg.value = "Your account has been deactivated";
       }
-    } catch (error) {
-      console.error("Error:", error.message)
-      errMsg.value = "An error occurred"
+    } else {
+      errMsg.value =
+        "No account with that alumni number and password was found";
     }
+  } catch (error) {
+    console.error("Error:", error.message);
+    errMsg.value = "An error occurred";
   }
-  </script>
+};
+</script>
