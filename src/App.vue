dca3d00c62511f8b5f3aa9ce1fefdad082451765
Collin Coleto <collin.coleto@deepdata.com>
2024-05-21 12:00:43 +0800

    Upt

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
-    <router-view v-if="!isLoading" />
-    <Loading v-else />
+    <router-view v-if="!isLoading"></router-view>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuth } from './composables/useAuth';
 import Loading from './views/loading.vue';
 
 const isLoading = ref(true);
+const router = useRouter();
+
+const { isAuthenticated, checkAuth } = useAuth();
+
+checkAuth();
 
 onMounted(() => {
+  if (!isAuthenticated.value) {
+    router.push({ name: 'login' });
+  }
   isLoading.value = false;
 });
 </script>
+
