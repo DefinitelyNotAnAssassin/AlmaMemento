a929e053a06791a654db94f4a488f0c93aae5cb6
CollinColeto <165641783+CollinColeto@users.noreply.github.com>
2024-05-23 05:22:58 +0800

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBGADdsBQqBwE08FjBC0UyY6rRAAXPuTU4',
-  authDomain: 'almamemento-6c12e.firebaseapp.com',
-  projectId: 'almamemento-6c12e',
-  storageBucket: 'almamemento-6c12e.appspot.com',
-  messagingSenderId: '732279676471',
-  appId: '1:732279676471:web:ad1bef8499e4b3606769b2'
+  apiKey: "AIzaSyCV7PyJD_6x3_yI-Ow247qxrXTF5Q4w6Ro",
+  authDomain: "almamemento2.firebaseapp.com",
+  projectId: "almamemento2",
+  storageBucket: "almamemento2.appspot.com",
+  messagingSenderId: "699584866738",
+  appId: "1:699584866738:web:51fc91824accba66421cab",
+  measurementId: "G-RDED32M0BD"
 }
 
 const app = initializeApp(firebaseConfig);
