729b3c69ad15949d4a56373e865b9c5fd091d448
Emmanuel Ugaban <>
2024-05-23 02:17:02 +0800

    url

--- src/views/alumni-components/memento-sidebar.vue
+++ src/views/alumni-components/memento-sidebar.vue
@@ -109,7 +109,7 @@ const fetchUserData = async () => {
       photoURL: user.profilePicture,
     };
 
-    console.log("user" + user.profilePicture);
+    console.log("user" + userData.photoURL);
   } else {
     console.log("User not found");
   }
